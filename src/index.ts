0d53e07813761f4fc2867b6b838942c064b2212f
osama-sayah <157955955+osama-sayah@users.noreply.github.com>
2024-04-07 01:32:42 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,7 @@ const publisherChannel = new PublisherChannel();
 
 // CORS middleware
 app.use(cors({
-  origin: ['https://osama-sayah.github.io','https://final-project-gateway.onrender.com','https://osama-sayah.github.io/Final_Project_App'],
-  credentials: true
+  origin: ['https://osama-sayah.github.io','https://final-project-gateway.onrender.com','https://osama-sayah.github.io/Final_Project_App']
 }));
 
 // Routes
